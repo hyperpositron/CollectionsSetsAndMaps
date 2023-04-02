07d9ed5d09f5454e239d9d33eef2b36beb271a0a
Александр <hyperpositron@gmail.com>
2023-04-02 21:52:24 +0200

    task3

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        List<Integer> nums = generate(10);
+        List<Integer> nums = generateInts(20);
         System.out.println("Сгенерированные числа " + nums);
         task1(nums);
         task2(nums);
+        task3(List.of("test","Test","tEst","teSt","tesT","est","tet","tet","tet"));
     }
-    private static List<Integer> generate(int n) {
+
+    private static List<Integer> generateInts(int n) {
         List<Integer> list = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             list.add(RANDOM.nextInt(8));
@@ -20,6 +22,7 @@ public class Main {
 
         return list;
     }
+
 
     private static void task1(List<Integer> nums) {
         System.out.println("ЗАДАЧА №1");
@@ -44,6 +47,15 @@ public class Main {
         System.out.println("==================================================");
     }
 
-
+    private static void task3(List<String> words) {
+        System.out.println("ЗАДАЧА №3");
+        for (String word : words) {
+            if (Collections.frequency(words, word)==1) {
+                System.out.print(word + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("==================================================");
+    }
 
 }
