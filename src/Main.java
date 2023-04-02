5ee548adc922c6aacaa5e09fc02d326069b0c334
Александр <hyperpositron@gmail.com>
2023-04-02 19:36:39 +0200

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class Main {
+    private static final Random RANDOM = new Random();
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = generate(10);
+        System.out.println("Сгенерированные числа " + nums);
+        task1(nums);
+    }
+
+    private static void task1(List<Integer> nums) {
+        System.out.println("ЗАДАЧА №1");
+        for (Integer number : nums) {
+            if (number % 2 == 1) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("==================================================");
+    }
+
+    private static List<Integer> generate(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(RANDOM.nextInt(8));
+        }
+
+        return list;
     }
 }
